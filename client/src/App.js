e7fc2d3bc6322c4760124ff4b6a3c3dbbfc7d4c0
VARSHINI <varshinis.22cse@kongu.edu>
2025-03-17 03:41:47 +0530

    Add all project files and initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/home';
 import Login from './pages/login';
 import Signup from './pages/signup';
 import { Toaster } from "react-hot-toast";
-import ProtectedRouter from "./components/protectedRouter";
-import { useSelector } from "react-redux";
+import ProtectedRoute from "./components/protectedRouter";
 import Loader from "./components/loader";
+import { useSelector } from "react-redux";
+
 function App() {
-  const loader = useSelector(state => state.loaderReducer);
+  const { loader } = useSelector(state => state.loaderReducer);
   return (
-   <div>
-    <Toaster position="top-center" reverseOrder={false}/>
-    {loader && <Loader />}
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<ProtectedRouter></ProtectedRouter>}></Route>
-      <Route path="/login" element={<Login />}></Route>
-      <Route path="/signup" element={<Signup />}></Route>
-      </Routes>
+    <div>
+      <Toaster position="top-center" reverseOrder={false} />
+      { loader && <Loader /> }
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute> }>
+         
+          </Route>
+          <Route path="/login" element={<Login /> }></Route>
+          <Route path="/signup" element={<Signup /> }></Route>
+        </Routes>
       </BrowserRouter>
-   </div>
+    </div>
   );
 }
 
